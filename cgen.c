6ceea1416bfb1fe1e4e3167718038bdd8c785169
Gabriel Kenji de Almeida <gab_kenji@hotmail.com>
2020-08-08 18:45:27 -0300

    a

--- cgen.c
+++ cgen.c
@@ -6,9 +6,12 @@
 int location = 1;
 int tempReg = 0;
 
-static void cGen( TreeNode * tree, THead* intercode);
+static char* cGen( TreeNode * tree, THead* intercode);
 
 void genStmt(TreeNode *tree, THead* intercode){
+
+  TreeNode *aux1, *aux2, *aux3;
+  Address addr1, addr2, addr3;
 
   switch(tree -> kind.stmt){
     case ifK:
@@ -16,12 +19,25 @@ void genStmt(TreeNode *tree, THead* intercode){
     case whileK:
     break;
     case assignK:
+      aux1 = tree -> child[1];
+      cGen(aux1, intercode);
     break;
-    case varK:
+    case varK: ;
+
+      addr1 = initAddress(labelA, 0, tree -> attr.name, tree -> attr.scope);
+      addr2 = initAddress(labelA, 0, tree -> attr.scope, tree -> attr.scope);
+      addr3 = initAddress(numA, tree -> attr.len, NULL, NULL);
+
+      insereLista(intercode, addr1, addr2, addr3, allocOp, location);
+      location++;
+      
     break;
     case funcK:
+      aux2 = tree -> child[1];
+      cGen(aux2, intercode);
     break;
     case callK:
+
     break;
     case returnK:
     break;
@@ -29,50 +45,163 @@ void genStmt(TreeNode *tree, THead* intercode){
     break;
     case paramK:
     break;
-    case vectStmtK:
+    case vectStmtK: ;
+
+      addr1 = initAddress(labelA, 0, tree -> attr.name, tree -> attr.scope);
+      addr2 = initAddress(labelA, 0, tree -> attr.scope, tree -> attr.scope);
+      addr3 = initAddress(numA, tree -> attr.len, NULL, NULL);
+
+      insereLista(intercode, addr1, addr2, addr3, allocOp, location);
+      location++;
     break;
     
   }
 
 }
 
-void genExp(TreeNode *tree, THead *intercode){
+char* genExp(TreeNode *tree, THead *intercode){ //  a função retorna o ultimo registrador temporário utilizado ou NULL se nao utilizar
 
+  Address addr1, addr2, addr3;
   TreeNode *aux1, *aux2, *aux3;
 
   switch(tree -> kind.exp){
     case opK:
-    break;
-    case constK:
-    break;
-    case idK:
-    break;
-    case vectK:
+
+    aux1 = tree -> child[0];
+    aux2 = tree -> child[1];
+
+    char * reg1 = cGen(aux1, intercode);
+    char * reg2 = cGen(aux2, intercode);
+    char *reg3 = (char*)malloc(5*sizeof(char));
+    sprintf(reg3, "$t%d", tempReg);
+    tempReg++;
+    addr1 = initAddress(labelA, 0, reg3, tree -> attr.scope);
+    addr2 = initAddress(labelA, 0, reg1, tree -> attr.scope);
+    addr3 = initAddress(labelA, 0, reg2, tree -> attr.scope);
+
+    switch(tree -> attr.op){
+      case SOM:
+      insereLista(intercode, addr1, addr2, addr3, addOp, location);
+      location++;
+      break;
+
+      case SUB:
+      insereLista(intercode, addr1, addr2, addr3, subOp, location);
+      location++;
+      break;
+
+      case MUL:
+      insereLista(intercode, addr1, addr2, addr3, multOp, location);
+      location++;
+      break;
+
+      case DIV:
+      insereLista(intercode, addr1, addr2, addr3, divOp, location);
+      location++;
+      break;
+
+      case MENOR:
+      insereLista(intercode, addr1, addr2, addr3, ltOp, location);
+      location++;
+      break;
+
+      case MENORIGUAL:
+      insereLista(intercode, addr1, addr2, addr3, letOp, location);
+      location++;
+      break;
+
+      case MAIOR:
+      insereLista(intercode, addr1, addr2, addr3, gtOp, location);
+      location++;
+      break;
+
+      case MAIORIGUAL:
+      insereLista(intercode, addr1, addr2, addr3, getOp, location);
+      location++;
+      break;
+
+      case IGUALIGUAL:
+      insereLista(intercode, addr1, addr2, addr3, eqOp, location);
+      location++;
+      break;
+
+      case DIFERENTE:
+      insereLista(intercode, addr1, addr2, addr3, noteqOp, location);
+      location++;
+      break;
+    }
+    return reg3;
     break;
 
-    
+    case constK: ;
+      char *reg4 = (char*)malloc(5*sizeof(char));
+      sprintf(reg4, "$t%d", tempReg);
+      tempReg++;
+
+      addr1 = initAddress(labelA, 0, reg4, tree -> attr.scope);
+      addr2 = initAddress(numA, tree -> attr.val, NULL, NULL);
+      addr3 = initAddress(nop, 0, NULL, NULL);
+
+      insereLista(intercode, addr1, addr2, addr3, immedOp, location);
+      location++;
+      return reg4;
+
+    break;
+
+    case idK: ;
+      char *reg = (char*)malloc(5*sizeof(char));
+      sprintf(reg, "$t%d", tempReg);
+      tempReg++;
+      addr1 = initAddress(labelA, 0, reg, tree -> attr.scope);
+      addr2 = initAddress(labelA, 0, tree -> attr.name, tree -> attr.scope);
+      addr3 = initAddress(nop, 0, NULL, NULL);
+
+      insereLista(intercode, addr1, addr2, addr3, loadOp, location);
+      location++;
+      return reg;
+    break;
+
+    case vectK:
+      aux1 = tree -> child[0];
+      char *regt = cGen(aux1, intercode);
+      char *regVec = (char*)malloc(5*sizeof(char));
+      sprintf(regVec, "$t%d", tempReg);
+      tempReg++;
+
+      addr1 = initAddress(labelA, 0, regVec, tree -> attr.scope);
+      addr2 = initAddress(labelA, 0, tree -> attr.name, tree -> attr.scope);
+      addr3 = initAddress(labelA, 0, regt, tree -> attr.scope);
+
+      insereLista(intercode, addr1, addr2, addr3, loadOp, location);
+      location++;
+
+      return regVec;
+
+    break;
+
     case typeK:
     aux1 = tree -> child[0];
     cGen(aux1, intercode);
-    printf("Declaração de tipo\n");
     break;
-
   }
+  return NULL;
 }
 
-static void cGen( TreeNode * tree, THead* intercode){ 
+static char* cGen( TreeNode * tree, THead* intercode){ 
 	if (tree != NULL){ 
+    char * lastRegt;
 		switch (tree->nodekind) {
       		case statementK:
         		genStmt(tree, intercode);
         		break;
       		case expressionK:
-        		genExp(tree, intercode);
+        		lastRegt = genExp(tree, intercode);
         		break;
       		default:
         		break;
     }
     cGen(tree->sibling, intercode);
+    return lastRegt;
   }
 }
 
@@ -82,6 +211,7 @@ void codeGen(TreeNode * syntaxTree, char * codefile)
    strcat(s,codefile);
    THead * intercodeTAD = initLista();
    cGen(syntaxTree, intercodeTAD);
+   printIntercode(intercodeTAD);
 
 }
 
